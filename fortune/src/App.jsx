c90ccf3f40fc141ac95afded5009b6e45d3cdd06
komekoneko <kai.s20050701@gmail.com>
2026-09-22 22:26:24 +0900

    add input保存

--- fortune/src/App.jsx
+++ fortune/src/App.jsx
@@ -5,16 +5,21 @@ import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    const [input, setInput] = useState[""]
+
+  const fortuneFm = () => {
+
+  }
 
   return (
     <>
      <h1>今日の占い</h1>
      <label htmlFor="name">名前:</label>
-      <input type="text" id='name' placeholder='名前を入力してください'/>
+      <input type="text" id='name' value={input}  onChange={(e) => setInput(e.target.value)} placeholder='名前を入力してください'/>
       <label htmlFor="born">生年月日:</label>
       <input type="date" placeholder='年/月/日' />
-    <button>占いを開始</button>
+    <button onClick={fortuneFn}>占いを開始</button>
     </>
   )
 }
