b436da04b314f811c551b8abe2292cd614eb561b
komekoneko <kai.s20050701@gmail.com>
2026-09-23 23:27:47 +0900

    repair エラー

--- fortune/src/App.jsx
+++ fortune/src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
+    const [input, setInput] = useState("");
 
-    const [input, setInput] = useState[""]
+  // const fortuneFn = () => {
 
-  const fortuneFm = () => {
+  // }
 
-  }
 
   return (
     <>
@@ -19,7 +16,7 @@ function App() {
       <input type="text" id='name' value={input}  onChange={(e) => setInput(e.target.value)} placeholder='名前を入力してください'/>
       <label htmlFor="born">生年月日:</label>
       <input type="date" placeholder='年/月/日' />
-    <button onClick={fortuneFn}>占いを開始</button>
+    <button>占いを開始</button>
     </>
   )
 }
