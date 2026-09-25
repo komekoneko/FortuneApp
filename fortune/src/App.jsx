9f192be8d4c09d6947d63e876404f82832092942
komekoneko <kai.s20050701@gmail.com>
2026-09-25 22:35:23 +0900

    add inputの空対策

--- fortune/src/App.jsx
+++ fortune/src/App.jsx
@@ -4,9 +4,11 @@ import './App.css'
 function App() {
     const [input, setInput] = useState("");
 
-  // const fortuneFn = () => {
-
-  // }
+  const fortuneFn = () => {
+    if(input == ""){
+      return alert("何か入力してください")
+    }
+  }
 
 
   return (
@@ -16,7 +18,7 @@ function App() {
       <input type="text" id='name' value={input}  onChange={(e) => setInput(e.target.value)} placeholder='名前を入力してください'/>
       <label htmlFor="born">生年月日:</label>
       <input type="date" placeholder='年/月/日' />
-    <button>占いを開始</button>
+    <button onClick={fortuneFn}>占いを開始</button>
     </>
   )
 }
